4b0c2803a7a2160ca7b15fc2a305855e759370fb
FilmonTeweldeBirhan <filmontewelde415@gmail.com>
2023-12-03 09:47:15 -0800

    added routes to post

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
 
+/* =======================
+    /api/v1/posts/
+   /api/v1/posts/:postID  
+   ======================= */
+// Get All Posts
+router.get("/", getAllPosts);
+
+// Get Post
+router.get("/:postID", getPost);
+
 module.exports = router;
