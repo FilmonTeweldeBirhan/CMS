60b697f40adc8bec6c5a11996ccc463768ed6716
FilmonTeweldeBirhan <filmontewelde415@gmail.com>
2023-12-07 02:57:57 -0800

    added passport and session

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const express = require("express");
 const morgan = require("morgan");
 const postsRouter = require("./routes/postsRoutes");
 const categoryRouter = require("./routes/categoryRoutes");
+const userRouter = require("./routes/userRoutes");
 const globalErrorHandler = require("./controllers/errorController");
 const APPError = require("./utils/appError");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const app = express();
+
+/* =========== SETTING UP PUG  =========== */
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 // Parsing bodys
 app.use(express.json({ limit: "10kb" }));
@@ -20,12 +28,48 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.get("/home", (req, res) => {
-  res.send(`<h1>Yes I'M Here!</h1>`);
+/* =========== SESSION SETUP  =========== */
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.DATABASE_LOCAL,
+  collectionName: "sessions",
+});
+
+const options = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+  },
+};
+
+if (app.get("env") === "production") {
+  options.cookie.secure = true;
+}
+
+app.use(session(options));
+
+/* =========== PASSPORT AUTHENTICATION  =========== */
+// Need to require the passport configuration
+require("./controllers/passportConfig/passport");
+
+// Helps to keep persistance session in database
+app.use(passport.initialize());
+app.use(passport.session());
+
+// trying
+app.use((req, res, next) => {
+  // console.log("SESSION:", req.session);
+  // console.log("USER:", req.user);
+  console.log("isAuthenticated:", req.isAuthenticated());
+
+  next();
 });
 
 /* =========== ROUTES  =========== */
 app.use("/api/v1/posts", postsRouter);
+app.use("/api/v1/users", userRouter);
 app.use("/api/v1/categories", categoryRouter);
 
 /* =========== Error Handleres  =========== */
